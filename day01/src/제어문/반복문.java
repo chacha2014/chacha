a207863b9832e36af63b3240c0cf578ffc1f8c8d
chacha2014 <65230307+chacha2014@users.noreply.github.com>
2020-05-12 22:24:17 +0900

    Add files via upload

---
+++ day01/src/제어문/반복문.java
@@ -0,0 +1,17 @@
+package 제어문;
+
+public class 반복문 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("내가 반복.");
+		}
+		//자동완성: 컨트롤 + 스페이스바
+		//while(true) {
+		//System.out.println("내가 반복.");
+		//System.out.println("나도 반복.");
+		//}
+	}
+
+}
